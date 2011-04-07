4762b1e03bd67a468bdcf64cf2f44fff7b41b2e3
Pat Hawks <pat@pathawks.com>
2011-04-07 20:25:29 +0000

    Fixed issue with Print function
    git-svn-id: http://plugins.svn.wordpress.org/dirty-suds-export-to-indesign/tags/1.03@370351 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ export.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name: DirtySuds - Export to InDesign
+Plugin URI: http://dirtysuds.com
+Description: Export a post as Adobe TaggedText for import to InDesign
+Author: pathawks
+Version: 1.03
+Author URI: http://www.pathawks.com
+
+Updates:
+1.03 - Indent Text
+1.02 - Faster. Now removes shortcodes
+1.01 - Better support for special characters
+1.00.20110222 - First Version
+
+  Copyright 2011 Pat Hawks  (email : pat@pathawks.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* Define the custom box */
+add_action('admin_init', 'dirtysuds_export_html_box', 1);
+add_action('single_template', 'dirtysuds_export_html');
+
+/* Adds a box to the main column on the Post and Page edit screens */
+function dirtysuds_export_html_box() {
+    add_meta_box( 'dirtysuds_export_html', 'Export Post', 'dirtysuds_export_html_box_inner', 'post', 'side','high' );
+}
+
+/* Prints the box content */
+function dirtysuds_export_html_box_inner($post, $metabox) {
+
+	echo '<a class="button" style="float:right;margin-top:-3px" target="_blank" href="'.get_permalink($post->ID).'?export=print'.'">Print</a>';
+	echo '<a class="button" href="'.get_permalink($post->ID).'?export=taggedtext'.'">Export to InDesign</a> ';
+}
+
+function dirtysuds_export_html($single_template) {
+if (strpos(' '.$_SERVER['QUERY_STRING'],'export=taggedtext')) {
+ob_end_clean();
+return dirname( __FILE__ ) .'/taggedtext.php';
+} else if (strpos(' '.$_SERVER['QUERY_STRING'],'export=print')) {
+ob_end_clean();
+return dirname( __FILE__ ) .'/print.php';
+} else {
+return $single_template;
+}
+}
---
+++ print.php
@@ -0,0 +1,41 @@
+<?php
+if (have_posts()): while (have_posts()): the_post();
+ob_end_clean();
+?><html><head><title><?php the_title_attribute(); ?></title>
+<style type="text/css">
+html * {
+font-size:14pt;
+line-height:28pt;
+font-family: Times, serif;
+}
+
+p {
+text-indent: 2em;
+margin:0;
+padding:0;
+}
+
+#author {
+width:100%;
+text-align:right;
+margin-bottom: 4em;
+font-size:16pt;
+line-height: 22pt;
+}
+#author strong {
+font-size:20pt;
+}
+</style>
+</head><body>
+<div id="author">
+<strong><?php the_title_attribute(); ?></strong><br />
+<?php the_author_firstname() ?> <?php the_author_lastname() ?><br />
+<?php echo get_the_author_email() ?>
+</div>
+<?php echo apply_filters('the_content',strip_shortcodes(make_url_footnote(get_the_content()))); ?>
+<script>
+window.print();
+</script>
+</body></html><?php
+endwhile; endif;
+exit();
