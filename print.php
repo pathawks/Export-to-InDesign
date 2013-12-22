1c9977bc8dfc97a6f503e0b1d3a46e29815644ba
Pat Hawks <pat@pathawks.com>
2013-12-22 01:01:00 +0000

    Merging changes from North Bay Business Journal
    git-svn-id: http://plugins.svn.wordpress.org/dirty-suds-export-to-indesign/trunk@826815 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- print.php
+++ print.php
@@ -11,7 +11,10 @@ function displayMetaField($title,$data) {
 		echo ' <strong>',$title,':</strong> <span style="color:red;">none specified</span><br />',PHP_EOL;
 }
 
-?><html><head><title><?php the_title_attribute(); ?></title>
+?><!DOCTYPE html>
+<html>
+<head><title><?php the_title_attribute(); ?></title>
+<meta name="robots" content="noindex" />
 <style type="text/css">
 @media screen, print{ 
 html * {
@@ -58,113 +61,97 @@ h3, h4 { margin-bottom:0; }
 </style>
 </head><body onload='window.print()'>
 <div id="header">
-	<?php
+<?php
+	displayMetaField(
+		'Print headline',
+		get_post_meta($post->ID, 'story_print_headline', true)
+	);
+
+	displayMetaField(
+		'Web headline',
+		$post->post_title
+	);
+
+	displayMetaField(
+		'Subhead/deck',
+		get_post_meta($post->ID, 'sub_head', true)
+	);
+
+	if(get_post_custom_values("guest_author"))
 		displayMetaField(
-			'Print headline',
-			get_post_meta($post->ID, 'story_print_headline', true)
+			'By',
+			get_post_custom_values("guest_author")
+		);
+	else
+		displayMetaField(
+			'By',
+			get_the_author_meta('first_name').' '.get_the_author_meta('last_name')
 		);
 
-		displayMetaField(
-			'Web headline',
-			$post->post_title
-		);
+	displayMetaField(
+		'Email',
+		get_the_author_meta('user_email')
+	);
 
-		displayMetaField(
-			'Subhead/deck',
-			get_post_meta($post->ID, 'sub_head', true)
-		);
+	displayMetaField(
+		'Modified',
+		get_the_date('F j, Y<\s\u\b\>@\<\/\s\u\b\>g:i<\s\u\b\>A\<\/\s\u\b\>')
+	);
 
-		if(get_post_custom_values("guest_author"))
-			displayMetaField(
-				'By',
-				get_post_custom_values("guest_author")
-			);
-		else
-			displayMetaField(
-				'By',
-				get_the_author_meta('first_name').' '.get_the_author_meta('last_name')
-			);
-
-		displayMetaField(
-			'Email',
-			get_the_author_meta('user_email')
-		);
-
-		displayMetaField(
-			'Modified',
-			get_the_date('F j, Y<\s\u\b\>@\<\/\s\u\b\>g:i<\s\u\b\>A\<\/\s\u\b\>')
-		);
-
-		displayMetaField(
-			'Status',
-			strtr(
-				get_post_status( $ID ),
-				array(
-					'publish' => 'Published',
-					'pending' => 'Pending review by an editor',
-					'draft' => 'Draft',
-					'assigned' => 'Assigned to a writer',
-					'future' => 'Scheduled to post in the future',
-					'sent-to-production' => 'Sent to Production',
-					'pitch' => 'Story idea',
-					'waiting-for-feedback' => 'Waiting for editor feedback',
-				)
+	displayMetaField(
+		'Status',
+		strtr(
+			get_post_status( $ID ),
+			array(
+				'publish' => 'Published',
+				'pending' => 'Pending review by an editor',
+				'draft' => 'Draft',
+				'assigned' => 'Assigned to a writer',
+				'future' => 'Scheduled to post in the future',
+				'sent-to-production' => 'Sent to Production',
+				'pitch' => 'Story idea',
+				'waiting-for-feedback' => 'Waiting for editor feedback',
 			)
-		);
+		)
+	);
 
-		displayMetaField(
-			'Production notes',
-			get_post_meta($post->ID, 'story_production_notes', true)
-		);
+	displayMetaField(
+		'Production notes',
+		get_post_meta($post->ID, 'story_production_notes', true)
+	);
 
-		displayMetaField(
-			'Captions',
-			get_post_meta($post->ID, 'story_captions', true)
-		);
+	displayMetaField(
+		'Captions',
+		get_post_meta($post->ID, 'story_captions', true)
+	);
 
-		displayMetaField(
-			'Story length',
-			str_word_count(strip_tags($post->post_content)).'<sub>words</sub> '.
-			round((str_word_count(strip_tags($post->post_content)) / 35), 1).
-			'<sub>inches</sub>'
-		);
+	displayMetaField(
+		'Story length',
+		str_word_count(strip_tags($post->post_content)).'<sub>words</sub> '.
+		round((str_word_count(strip_tags($post->post_content)) / 35), 1).
+		'<sub>inches</sub>'
+	);
 
-		displayMetaField(
-			'Filename',
-			preg_replace(
-				"/[^a-zA-Z0-9\-_]/","",
-				get_the_author_lastname()
-					.'-'.str_replace(' ','_',basename(get_permalink()))
-			).'.txt'
-		);
-
-/*
-		// ADD ANOTHER AUTHOR TO BYLINE AND LINK TO THEIR AUTHOR PAGE
-		$add_author = get_post_custom_values("add_author");
-		if ( $add_author ) {
-			//	$wpdb->show_errors();
-			foreach($add_author as $key => $value) { 
-				// QUERY FOR DISPLAY NAME BY SEARCHING FOR NICENAME
-				$q_author = $wpdb->get_var("SELECT display_name FROM $wpdb->users WHERE user_nicename = '$add_author[$key]'");
-				// REMOVE ANY SPECIAL CHARS THAT MAY BE IN THE NICENAME ( FOR LINK )
-				$remove_chars = array(".",",");
-				$q_author = str_replace($remove_chars, "", $q_author);
-				$q_author_link = strtolower(str_replace(" ", "-", $q_author));
-				if ($q_author) {
-					echo ' and ',$q_author;
-				} else {
-					echo "and $add_author[$key]";
-				}
-			} 
-		} */
-	?>
+	displayMetaField(
+		'Filename',
+		preg_replace(
+			"/[^a-zA-Z0-9\-_]/","",
+			get_the_author_lastname()
+				.'-'.str_replace(' ','_',basename(get_permalink()))
+		).'.txt'
+	);
+?>
 </div>
 <?php
-	$content = apply_filters('the_content', $content);
+	$content = get_the_content();
 	$content = str_replace(']]>', ']]>', $content);
-	echo trim(apply_filters('the_content',strip_shortcodes(make_url_footnote(get_the_content()))));
+	$content = get_the_content();
+	$content = make_url_footnote( $content );
+	$content = strip_shortcodes( $content );
+	$content = apply_filters('the_content', $content );
+	$content = trim( $content );
+	echo $content;
 ?>
-
 </body>
 </html>
 <?php
